97b08dc666b6bec42e9184f6cdc92f376eb3da1a
kheireddine <aberbache_k@yahoo.ca>
2017-06-12 10:06:05 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shifter2::Application.config.secret_token = 'fb3dd3a8ff5d59f795e49cc93250ee31090e74b8c2baa6876a6f05acc212f4b14658b5c306ac819ffb61fed626b53637cff360c49b4f716c6238beea8133f3fd'
