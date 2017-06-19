012d268a80e9a1dbe43f7bc3b4e35a5fa6fcf1d4
kheireddine <aberbache_k@yahoo.ca>
2017-06-19 12:27:16 -0400

    Added missing init_roles and validation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,9 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   include SessionsHelper
 
-  before_filter :sign_in_if_not_logged
+  ROLES = %w(admin manager other)
+
+  before_filter :sign_in_if_not_logged, :init_roles
 
 
   # Force signout to prevent CSRF attacks
@@ -18,5 +20,12 @@ class ApplicationController < ActionController::Base
       (redirect_to signin_path, :flash => { :error => "You must signin first"}) unless signed_in?
     end
 
+    def init_roles
+      if Role.all.blank?
+        ROLES.each do |role|
+          Role.create(name: role)
+        end
+      end
+    end
 
 end
--- app/models/role.rb
+++ app/models/role.rb
@@ -1,3 +1,7 @@
 class Role < ActiveRecord::Base
   attr_accessible :name
+
+  ROLES = %w(admin manager other)
+  validates :name, presence: true, uniqueness: true, :inclusion => {:in => ROLES}
+
 end
