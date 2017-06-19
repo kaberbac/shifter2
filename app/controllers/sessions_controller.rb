abdd1baf9dd18eba10c4f11cf96b29dbe7cd976e
kheireddine <aberbache_k@yahoo.ca>
2017-06-19 14:02:01 -0400

    after login, redirect and menu change according if admin or not

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,11 @@ class SessionsController < ApplicationController
     user = User.find_by_email(params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       sign_in user
-      redirect_to user_shifts_path(user)
+      if user.is_role?('admin')
+        redirect_to admin_users_path
+      else
+        redirect_to user_shifts_path(user)
+      end
     else
       flash.now[:error] = 'Invalid email/password combination'
       render 'new'
