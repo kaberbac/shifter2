8b0c8f6c155b596ecbfe2e4f52dfa3d9f88ac088
Kheireddine <aberbache_k@yahoo.ca>
2017-06-21 22:26:47 -0400

    - seeds doesnt create admin user if 1 exists - seeds create admin user in  case no other user is admin or give admin role to default admin if it doesnt have

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,20 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
 
-unless User.find_by_email("admin@email.com").present?
-  admin_user = User.create!(first_name: "iam", last_name: "theboss", email: "admin@email.com", password: "123456")
-  puts "-----------------------------------------"
-  puts "#{admin_user.full_name} has been created."
-  puts "-----------------------------------------"
-  admin_user.user_roles.create!(role_name: Role.get_admin!)
-  puts "-----------------------------------------"
-  puts "#{admin_user.full_name} has been granted with admin role."
-  puts "-----------------------------------------"
+
+unless UserRole.where(role_name: 'admin').exists?
+  admin_user = User.find_by_email("admin@email.com")
+  unless admin_user.present?
+    admin_user = User.create!(first_name: "iam", last_name: "theboss", email: "admin@email.com", password: "123456")
+    puts "-----------------------------------------"
+    puts "#{admin_user.full_name} has been created."
+    puts "-----------------------------------------"
+  end
+
+  if admin_user.present?
+    admin_user.user_roles.create!(role_name: Role.get_admin!)
+    puts "-----------------------------------------"
+    puts "#{admin_user.full_name} has been granted with admin role."
+    puts "-----------------------------------------"
+  end
 end
